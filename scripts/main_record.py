fafe73cf33e1cdc8b964875f352b0734d2498947
Andreas <andreas.verleysen@gmail.com>
2022-07-04 13:42:46 +0200

    append recording script

--- scripts/main_record.py
+++ scripts/main_record.py
@@ -1,18 +1,45 @@
-import cv2
-import numpy as np
-import pyzed.sl as sl
 import sys
+import pyzed.sl as sl
+from signal import signal, SIGINT
 
-# Create a ZED camera object
-zed = sl.Camera()
+cam = sl.Camera()
 
-# Enable recording with the filename specified in argument
-output_path = sys.argv[0]
-err = zed.enable_recording(output_path, sl.SVO_COMPRESSION_MODE.H264)
+def handler(signal_received, frame):
+    cam.disable_recording()
+    cam.close()
+    sys.exit(0)
 
-while !exit_app :
-    # Each new frame is added to the SVO file
-    zed.grab()
+signal(SIGINT, handler)
 
-# Disable recording
-zed.disable_recording()
+def main():
+    if not sys.argv or len(sys.argv) != 2:
+        print("Only the path of the output SVO file should be passed as argument.")
+        exit(1)
+
+    init = sl.InitParameters()
+    init.camera_resolution = sl.RESOLUTION.HD720
+    init.depth_mode = sl.DEPTH_MODE.NONE
+
+    status = cam.open(init)
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit(1)
+
+    path_output = sys.argv[1]
+    recording_param = sl.RecordingParameters(path_output, sl.SVO_COMPRESSION_MODE.H264)
+    err = cam.enable_recording(recording_param)
+    if err != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit(1)
+
+    runtime = sl.RuntimeParameters()
+    print("SVO is Recording, use Ctrl-C to stop.")
+    frames_recorded = 0
+
+    while True:
+        if cam.grab(runtime) == sl.ERROR_CODE.SUCCESS :
+            frames_recorded += 1
+            print("Frame count: " + str(frames_recorded), end="\r")
+
+if __name__ == "__main__":
+    main()
